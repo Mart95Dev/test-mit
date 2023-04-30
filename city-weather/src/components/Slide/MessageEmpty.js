dc70a2584ca51034740d8e46686e4fc40a15d2d1
Martial Wehrling <wehrling@free.fr>
2023-04-30 09:06:44 +0200

    🚧 F01: SLIDE> Create <MessageEmpty/> when Panel is empty

---
+++ city-weather/src/components/Slide/MessageEmpty.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MessageEmpty() {
+  return (
+    <div className="text-[#393838]">
+      <h1>Veuillez faire une recherche pour que les données s'affichent !</h1>
+    </div>
+  );
+}
+
+export default MessageEmpty;
